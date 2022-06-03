dddd93d927df5500eec45f14ac855b0d850797ec
nyuun <myfd642@gmail.com>
2022-06-03 13:43:56 +0000

    効率的に標本平均を算出

--- ch01/avg.py
+++ ch01/avg.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 np.random.seed(0)
-rewards = []
+
+Q = 0
 
 for n in range(1, 11):
     reward = np.random.rand()
-    rewards.append(reward)
-    Q = sum(rewards) / n
+    # Q = Q + (reward - Q) / n
+    Q += (reward - Q) / n
     print(Q)
