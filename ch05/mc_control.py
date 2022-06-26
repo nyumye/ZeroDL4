d75e212cfd4b5fe224a038d6da13ddfe0034d1f0
nyuun <myfd642@gmail.com>
2022-06-26 15:26:11 +0000

    ε-greedy法の実装

--- ch05/mc_control.py
+++ ch05/mc_control.py
@@ -39,11 +39,13 @@ class McAgent:
 
             self.pi[state] = greedy_probs(self.Q, state)
 
-def greedy_probs(Q, state, action_size=4):
+# 状態sにおける各行動の取られる確率を返す．
+def greedy_probs(Q, state, epsilon=0, action_size=4):
     qs = [Q[(state, action)] for action in range(action_size)]
     max_action = np.argmax(qs)
 
-    action_probs = {action: 0.0 for action in range(action_size)}
-    action_probs[max_action] = 1
+    base_prob = epsilon / action_size
+    action_probs = {action: base_prob for action in range(action_size)}
+    action_probs[max_action] += (1 - epsilon)
     return action_probs
 
